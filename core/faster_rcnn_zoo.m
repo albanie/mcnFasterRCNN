aedb907589db886b921dcca5cd12e69f3f16c793
albanie <albanie@robots.ox.ac.uk>
2017-07-28 16:26:31 +0100

    clean up

--- core/faster_rcnn_zoo.m
+++ core/faster_rcnn_zoo.m
@@ -1,46 +1,43 @@
 function net = faster_rcnn_zoo(modelName)
+%FASTER_RCNN_ZOO - load detector by name
+%  FASTER_RCNN_ZOO(MODELNAME) - loads a detector by its given name. 
+%  If it cannot be found on disk, it will be downloaded via the world
+%  wide web.
+%
+% Copyright (C) 2017 Samuel Albanie 
+% All rights reserved.
 
-modelNames = {
-  'faster-rcnn-vggvd-pascal', ...
-  'faster-rcnn-vggvd-coco'...
-} ;
+  modelNames = {
+    'faster-rcnn-vggvd-pascal', ...
+    'faster-rcnn-vggvd-coco'...
+  } ;
 
-msg = sprintf('%s: unrecognised model', modelName) ;
-assert(ismember(modelName, modelNames), msg) ;
-modelDir = fullfile(vl_rootnn, 'data/models-import') ;
-modelPath = fullfile(modelDir, sprintf('%s.mat', modelName)) ;
-
-if ~exist(modelPath, 'file')
-  fetchModel(modelName, modelPath) ;
-end
-
-net = dagnn.DagNN.loadobj(load(modelPath)) ;
+  msg = sprintf('%s: unrecognised model', modelName) ;
+  assert(ismember(modelName, modelNames), msg) ;
+  modelDir = fullfile(vl_rootnn, 'data/models-import') ;
+  modelPath = fullfile(modelDir, sprintf('%s.mat', modelName)) ;
+  if ~exist(modelPath, 'file'), fetchModel(modelName, modelPath) ; end
+  net = dagnn.DagNN.loadobj(load(modelPath)) ;
 
 % ---------------------------------------
 function fetchModel(modelName, modelPath)
 % ---------------------------------------
 
-waiting = true ;
-prompt = sprintf(strcat('%s was not found at %s\nWould you like to ', ...
-            ' download it from THE INTERNET (y/n)?\n'), modelName, modelPath) ;
+  waiting = true ;
+  prompt = sprintf(strcat('%s was not found at %s\nWould you like to ', ...
+          ' download it from THE INTERNET (y/n)?\n'), modelName, modelPath) ;
 
-while waiting
-  str = input(prompt,'s') ;
-  switch str
-    case 'y'
-      % ensure target directory exists
-      if ~exist(fileparts(modelPath), 'dir')
-        mkdir(fileparts(modelPath)) ;
-      end
-
-      fprintf(sprintf('Downloading %s ... \n', modelName)) ;
-      baseUrl = 'http://www.robots.ox.ac.uk/~albanie/models/faster' ;
-      url = sprintf('%s/%s.mat', baseUrl, modelName) ;
-      urlwrite(url, modelPath) ;
-      return ;
-    case 'n'
-      throw(exception) ;
-    otherwise
-      fprintf('input %s not recognised, please use `y` or `n`\n', str) ;
+  while waiting
+    str = input(prompt,'s') ;
+    switch str
+      case 'y'
+        if ~exist(fileparts(modelPath), 'dir'), mkdir(fileparts(modelPath)) ; end
+        fprintf(sprintf('Downloading %s ... \n', modelName)) ;
+        baseUrl = 'http://www.robots.ox.ac.uk/~albanie/models/faster' ;
+        url = sprintf('%s/%s.mat', baseUrl, modelName) ;
+        urlwrite(url, modelPath) ;
+        return ;
+      case 'n', throw(exception) ;
+      otherwise, fprintf('input %s not recognised, please use `y/n`\n', str) ;
+    end
   end
-end
--- core/getExpNameFRCNN.m
+++ core/getExpNameFRCNN.m
@@ -1,15 +1,18 @@
 function expName = getExpNameFRCNN(mopts, dopts) 
-%GETEXPNAMEFRCNN defines a naming strategy for each experiment, 
-% depending on the options used during training
+%GETEXPNAMEFRCNN create an appropriate experiment name  
+%   GETEXPNAMEFRCNN(MOPTS, DOPTS) defines a naming strategy for each 
+%   experiment, based on the model options MOPTS and data options DOPTS
+%   provided
+%
+% Copyright (C) 2017 Samuel Albanie 
+% All rights reserved.
 
-if dopts.useValForTraining, subset = 'vt' ; else, subset = 't' ; end
-args = {mopts.type, dopts.name, subset, mopts.batchSize, mopts.architecture} ;
-expName = sprintf('%s-%s-%s-%d-%s', args{:}) ; 
-
-if dopts.flipAugmentation, expName = [ expName '-flip' ] ; end
-if dopts.patchAugmentation, expName = [ expName '-patch' ] ; end
-if dopts.distortAugmentation, expName = [ expName '-distort' ] ; end
-
-if dopts.zoomAugmentation
-    expName = [ expName sprintf('-zoom-%d', dopts.zoomScale) ] ;
-end
+  if dopts.useValForTraining, subset = 'vt' ; else, subset = 't' ; end
+  args = {mopts.type, dopts.name, subset, mopts.batchSize, mopts.architecture} ;
+  expName = sprintf('%s-%s-%s-%d-%s', args{:}) ; 
+  if dopts.flipAugmentation, expName = [ expName '-flip' ] ; end
+  if dopts.patchAugmentation, expName = [ expName '-patch' ] ; end
+  if dopts.distortAugmentation, expName = [ expName '-distort' ] ; end
+  if dopts.zoomAugmentation
+      expName = [ expName sprintf('-zoom-%d', dopts.zoomScale) ] ;
+  end
