98b0eab734d774bdc6f0b39457c07ce5b09a3776
albanie <albanie@robots.ox.ac.uk>
2017-08-12 10:10:59 +0100

    clean up

--- setup_mcnFasterRCNN.m
+++ setup_mcnFasterRCNN.m
@@ -10,6 +10,3 @@ function setup_mcnFasterRCNN()
   addpath(root, [root '/matlab'], [root '/pascal'], [root '/core']) ;
   addpath([root '/pascal/helpers'], [root '/coco'], [root '/misc']) ;
   addpath([vl_rootnn '/examples/fast_rcnn/bbox_functions']) ;
-
-  % (only needed for tuning purposes)
-  addpath([root '/train_analysis']) ;
